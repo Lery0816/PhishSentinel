88194b1b45437d7107b48ce1d28b67a71cfbf8e3
Yifan Liu <ihave6cats2022@gmail.com>
2025-04-01 21:55:34 +1100

    use /score

--- src/main/java/com/uts/controller/DomainController.java
+++ src/main/java/com/uts/controller/DomainController.java
@@ -5,7 +5,6 @@ import com.uts.service.DomainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import com.uts.pojo.Domain;
 
 @Controller
 @ResponseBody
@@ -13,19 +12,6 @@ import com.uts.pojo.Domain;
 public class DomainController {
     @Autowired
     private DomainService domainService;
-
-    @PostMapping("/check")
-    public RiskResult checkDomain(@RequestBody Domain domain) {
-        String domainStr = domain.getDomainName();
-        RiskResult riskResult = new RiskResult();
-        riskResult.setListScore(domainService.checkList(domainStr));
-        riskResult.setStructureScore(domainService.analyzeDomainStructure(domainStr));
-        riskResult.setSslScore(domainService.calculateSSLScore(domainStr));
-        domainService.checkWhoisInfo(domainStr);
-        riskResult.setWhoisScore(domainService.calculateWhoisScore(domainStr));
-        riskResult.calculateRiskLevel();
-        return riskResult;
-    }
 
     @GetMapping("/score")
     public RiskResult scoreCheck(@RequestParam String domain) {
