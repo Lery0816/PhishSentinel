d0af57a35d03684877658b570d961b337a7d7254
Yifan Liu <ihave6cats2022@gmail.com>
2025-04-01 16:27:13 +1100

    move to to /api

--- src/main/java/com/uts/controller/UserController.java
+++ src/main/java/com/uts/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequestMapping("/api")
 public class UserController {
     @Autowired
     private UserService userService;
